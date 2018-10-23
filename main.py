f7f67fb987827c9a26973fbc6d62a42b68af44d3
aza <aza@lainventoria.com.ar>
2018-10-23 22:27:23 +0200

    make project configurable

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from data import *
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+project = 'objects'
 
 data_gen_args = dict(rotation_range=0.2,
                     width_shift_range=0.05,
@@ -11,12 +12,16 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 
+train_path = f"data/{project}/train"
+myGene = trainGenerator(2, train_path, 'image','label', data_gen_args, save_to_dir = None)
+
+save_path = f"unet_{project}.hdf5"
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+model_checkpoint = ModelCheckpoint(save_path, monitor='loss', verbose=1, save_best_only=True)
+model.fit_generator(myGene,steps_per_epoch=100,epochs=1,callbacks=[model_checkpoint])
 
-testGene = testGenerator("data/membrane/test")
+test_path = f"data/{project}/test"
+testGene = testGenerator(test_path)
 results = model.predict_generator(testGene,30,verbose=1)
 saveResult("data/membrane/test",results)
